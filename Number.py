b8d9ff5f30977441d1dd3a9763c5c025c764097e
Mano <mano.26q@gmail.com>
2022-01-19 14:12:28 +0530

    Update Number.py
    added the WIN case

--- Number.py
+++ Number.py
@@ -9,10 +9,13 @@ print("3.HOT indicates that the number you guess is above the actual number")
 guess_no=int(input("Enter the number you guessed: "))
 actual_no=randint(1,10**3)
 c=1
-while c:
+while c==1:
     if guess_no>actual_no:
         print("HOT!")
     elif guess_no<actual_no:
         print("COLD!")
     else:
-        print("YAY! Congratulations!! You've guess the correct number")
+        print("YAY! Congratulations!! You've guessed the correct number")
+        print("If you wanna try again, click y or click n to quit")
+        d=input("Enter your choice:")
+        
