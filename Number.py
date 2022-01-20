952072754998e693eae1b86e230a975f1fc7c30b
Mano <mano.26q@gmail.com>
2022-01-20 18:46:21 +0530

    Update Number.py
    added try feature. need to work on wide no. of input schema

--- Number.py
+++ Number.py
@@ -6,10 +6,20 @@ print("The rules:")
 print("1.Enter a number between 0 and 1000")
 print("2.COLD indicates that the number you guessed is below the actaul number.")
 print("3.HOT indicates that the number you guess is above the actual number")
-guess_no=int(input("Enter the number you guessed: "))
+guess_no=input("Enter the number you guessed: ")
 actual_no=randint(1,10**3)
 c=1
 while c==1:
+    try:
+        c=int(guess_no)
+    except ValueError:
+        if guess_no=='q':
+            print('Exiting window. See you next time!')
+            break
+        else:
+            print('Invalid Input')
+            digit=input('How many decimal places would you want for Pi to have?')
+            a=1
     if guess_no>actual_no:
         print("HOT!")
     elif guess_no<actual_no:
