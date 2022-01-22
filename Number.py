5d1a96d58b3635a856a02346e9342d61347e7e58
Mano <mano.26q@gmail.com>
2022-01-22 19:30:24 +0530

    Update Number.py
    added no of trails//

--- Number.py
+++ Number.py
@@ -6,8 +6,10 @@ print("The rules:")
 print("1.Enter a number between 0 and 1000")
 print("2.COLD indicates that the number you guessed is below the actaul number.")
 print("3.HOT indicates that the number you guess is above the actual number")
+print("4.The no. of trials will be displayed and you can beat your Highscore.")
 guess_no=input("Enter the number you guessed: ")
 actual_no=randint(1,10**3)
+count=0
 c=1
 while c==1:
     try:
@@ -24,16 +26,23 @@ while c==1:
         #add more interactive message box    
         if guess_no>actual_no:
             print("HOT!")
+            count+=1
         elif guess_no<actual_no:
             print("COLD!")
+            count+=1
         else:
+            count+=1
             print("YAY! Congratulations!! You've guessed the correct number")
+
+"""add print no of trials statement here
+work on no of trials algorith ok?"""
+
             print("If you wanna try again, click y or click n to quit")
             d=input("Enter your choice:")
             if d=='y' or d=="Y":
                 guess_no=int(input("Enter the number you guessed: "))
                 actual_no=randint(1,10**3)
             elif d=='n' or d=='N':
-
+                print("You clicked the exit option. Sad to see you go :(")
                 #add exit window message here
                 c=2
